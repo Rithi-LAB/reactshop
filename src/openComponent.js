32c664e4227b7ebc33158f0841e479d9a5d03777
Rithi-LAB <rithicoursera@gmail.com>
2024-12-10 13:41:42 +0530

    Update openComponent.js

--- src/openComponent.js
+++ src/openComponent.js
@@ -1,35 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './openComponent.css';
 import Categories from './categories.js';
 
 const OpenComponent = () => {
-    function start_shop(){
-        <Categories/>
+    
+    const [showCategories, setShowCategories] = useState(false);
+
+    function start_shop() {
+        setShowCategories(true); 
     }
 
     return (
         <>
-        <div className = "container">
-    <div className = "title"><b>
-    Home ELEGANCE_____ starts here...
-    </b></div>
-    <div className = "box">
-    High On Sale!!!
-    </div> </div>
-    <div className = "background-container">
-         we bring you an extensive selection of stylish, durable, and affordable furniture for every room of your home. From modern living room sofas to elegant bedroom sets, functional office desks to cozy outdoor lounge chairs, our website offers a wide range of options to suit your personal taste and lifestyle
-    </div>
-    
-    <div className = "start-button">
-    <button onClick = {start_shop}>
-    START SHOPPING
-    </button> </div>
-     
-    </>
- );
+            <div className="container">
+                <div className="title">
+                    <b>Home ELEGANCE_____ starts here...</b>
+                </div>
+                <div className="box">High On Sale!!!</div>
+            </div>
+            <div className="background-container">
+                We bring you an extensive selection of stylish, durable, and affordable furniture for every room of your home. From modern living room sofas to elegant bedroom sets, functional office desks to cozy outdoor lounge chairs, our website offers a wide range of options to suit your personal taste and lifestyle.
+            </div>
+            <div className="start-button">
+                <button onClick={start_shop}>START SHOPPING</button>
+            </div>
+
+          
+            {showCategories && <Categories />}
+        </>
+    );
 }
 
 export default OpenComponent;
+
 
 
 
