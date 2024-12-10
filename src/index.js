a95184b5adb174bf1ed5a97a47866511a879c9d0
Rithi-LAB <rithicoursera@gmail.com>
2024-12-10 13:32:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import OpenComponent from './openComponent'; 
 import './openComponent.css'; 
 import Categories from './categories.js';
-
+import Beds from './beds.js';
+import Sofas from './sofas.js';
+import Tables from './table.js';
+import Seaters from './seaters.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
